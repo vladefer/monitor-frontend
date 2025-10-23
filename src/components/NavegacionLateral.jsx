122b313086a4a4501b900e4faed5ce50a303bebc
Vladimir Ramirez <vladeramirez0607@gmail.com>
2025-10-22 19:00:08 -0500

    desarrollo

--- src/components/NavegacionLateral.jsx
+++ src/components/NavegacionLateral.jsx
@@ -1,9 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../auth/AuthProvider";
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../auth/AuthProvider"
 
 export default function NavegacionLateral({ expanded = false }) {
-   const { user, logout } = useAuth();
-   const navigate = useNavigate();
+   const { user, logout } = useAuth()
+   const navigate = useNavigate()
 
    const items = [
       { label: "Dashboard", icon: "pi pi-home", action: () => navigate("/") },
@@ -15,25 +15,17 @@ export default function NavegacionLateral({ expanded = false }) {
       { label: "ONAC", icon: "pi pi-receipt" },
       { label: 'Cerrar sesion', icon: "pi pi-sign-out", action: () => logout() },
       { separator: true },
-
-   ];
+   ]
 
    return (
-      <div
-         className={`h-screen transition-all duration-300 ease-in-out ${expanded ? "w-[18rem]" : "w-[5rem]"
-            } overflow-hidden rounded-2xl bg-white/100 backdrop-blur-md border border-black/5 shadow-sm`}
-         style={{ willChange: "width" }}
-      >
-         <nav className="flex flex-col justify-between h-full py-4">
+      <div className={`h-screen transition-all duration-300 ease-in-out ${expanded ? "w-[18rem]" : "w-[5rem]"} overflow-hidden rounded-2xl bg-white/100 backdrop-blur-md border border-black/5 shadow-sm`}
+      style={{ willChange: "width" }}>
 
+         <nav className="flex flex-col justify-between h-full py-4">
             <div>
                {/* Logo superior */}
                <div className="flex items-center justify-center py-0">
-                  <img
-                     src="/img/logo-marca.png"
-                     alt="Logo"
-                     className="w-16"
-                  />
+                  <img className="w-16" src="/img/logo-marca.png" alt="Logo"/>
                </div>
 
                {items
@@ -47,10 +39,7 @@ export default function NavegacionLateral({ expanded = false }) {
                            onClick={item.action}
                            className={`flex items-center gap-10 px-6 py-6 text-gray-800 w-full text-left hover:bg-gray-100 transition-colors duration-200 cursor-pointer`}
                         >
-                           <i
-                              className={`${item.icon} flex-shrink-0`}
-                              style={{ fontSize: "1.3rem" }}
-                           />
+                           <i className={`${item.icon} flex-shrink-0`} style={{ fontSize: "1.3rem" }}/>
                            <span className="whitespace-nowrap overflow-hidden text-[1.2rem] font-medium transition-all duration-200">
                               {item.label}
                            </span>
@@ -64,10 +53,7 @@ export default function NavegacionLateral({ expanded = false }) {
                <button
                   className="flex items-center gap-5 px-6 py-8 text-gray-800 w-full text-left hover:bg-cyan-100 hover:text-black transition-colors duration-200 cursor-pointer"
                >
-                  <i
-                     className="pi pi-user flex-shrink-0"
-                     style={{ fontSize: "1.3rem" }}
-                  />
+                  <i className="pi pi-user flex-shrink-0" style={{ fontSize: "1.3rem" }}/>
                   <span className="overflow-hidden text-[1.2rem] font-medium transition-all duration-200">
                      {user?.cliente_nombre}
                   </span>
@@ -75,5 +61,5 @@ export default function NavegacionLateral({ expanded = false }) {
             </div>
          </nav>
       </div>
-   );
+   )
 }
